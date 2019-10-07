130995ed7c288b1a2238f13245dcc46f5555110a
Juan Antonio Valino Garcia <juanval@edu.xunta.es>
2019-10-07 12:45:54 +0200

    Snapshot.

--- README.md
+++ README.md
@@ -1,96 +1,74 @@
-# Ansible <!-- this role name --> role
+# Ansible win_domain_groups_managedby role
 
-This is an [Ansible](http://www.ansible.com) role which <!-- brief description of the role goes here -->.
-
-## Requirements
-
-<!-- Any pre-requisites that may not be covered by Ansible itself or the role should be mentioned here. For instance, if the role uses the EC2 module, it may be a good idea to mention in this section that the boto package is required. For example: -->
-
-[Ansible 2.7+](http://docs.ansible.com/ansible/latest/intro_installation.html)
+This is an [Ansible](http://www.ansible.com) role which setup `managed by` field and ACL in windows active directory groups.
 
 ## Role Variables
 
-<!-- A description of the settable variables for this role should go here, including any variables that are in defaults/main.yml, vars/main.yml, and any variables that can/should be set via parameters to the role. Any variables that are read from other roles and/or the global scope (ie. hostvars, group vars, etc.) should be mentioned here as well. For example: -->
-
 A list of all the default variables for this role is available in `defaults/main.yml`.
-
-The role also setups the following facts:
-
-- `thisrole_fact1`: description of the fact
-- `thisrole_fact2`: description of the fact
-- `thisrole_factN`: description of the fact
-
-## Filters
-
-<!-- A description of the filters provided by the role should go here. For example: -->
-
-The role provides these filters:
-
-- `thisrole_filter1`: description of the filter
-- `thisrole_filter2`: description of the filter
-- `thisrole_filterN`: description of the filter
 
 ## Modules
 
-<!-- A description of the modules provided by the role should go here. For example: -->
-
 The role provides these modules:
 
-- `thisrole_module1`: description of the module
-- `thisrole_module2`: description of the module
-- `thisrole_moduleN`: description of the module
+- `win_domain_groups_managedby`: which setup `managed by` field and ACL in a windows active directory single group.
 
-## Tests
 
-<!-- A description of the tests provided by the role should go here. For example: -->
-
-The role provides these tests:
-
-- `thisrole_test1`: description of the test
-- `thisrole_test2`: description of the test
-- `thisrole_testN`: description of the test
-
-## Dependencies
-
-<!-- A list of other roles hosted on Galaxy should go here, plus any details in regards to parameters that may need to be set for other roles, or variables that are used from other roles. For example: -->
-
-- [amtega.check_platform](https://galaxy.ansible.com/amtega/check_platform)
-- [amtega.proxy_client](https://galaxy.ansible.com/amtega/proxy_client)
-- [amtega.packages](https://galaxy.ansible.com/amtega/packages)
+- `main.yml`: tests main use cases of the role and library.
 
 ## Usage
-
-<!-- Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too. For example: -->
 
 This is an example playbook:
 
 ```yaml
 ---
 
-- hosts: all
-  roles:
-    - role: thisrole
-      thisrole_var1: value1
-      thisrole_var2: value2
-      thisrole_varN: valuen
+  - hosts: windows_ad_computer
+    roles:
+      - role: amtega.win_domain_groups_managedby
+    vars:
+      win_domain_groups_managedby_defaults:
+        domain_server: windows_dc_2
+
+      win_domain_groups_managedby:
+
+        - group: app1_group
+          manager: app_admin
+          manage_enabled: yes
+          state: present
+
+        - group: app2_group
+          manager: app_admin
+          manage_enabled: no
+          state: present
+
+        - group: app3_group
+          manager: app_admin
+          state: absent
 ```
 
 ## Testing
 
-<!-- A description of how to run tests of the role if available. For example: -->
+To run test you must pass in the command line the variable `win_domain_groups_managedby_tests_host` pointing to a windows host fulfilling the ansible requirements documented in https://docs.ansible.com/ansible/latest/user_guide/windows_setup.html. Also, you must define in the inventory for this host the necessary variables to connect.
 
-Tests are based on docker containers. You can setup docker engine quickly using the playbook `files/setup.yml` available in the role [amtega.docker_engine](https://galaxy.ansible.com/amtega/docker_engine).
+Additionally the tests requires the following set of variables that can be defined in the inventory or passed in the command line:
 
-Once you have docker, you can run the tests with the following commands:
+- `win_domain_groups_managedby_ou`: OU where the test groups reside.
+- `win_domain_groups_managedby_group`: Group to be managed during the tests
+- `win_domain_groups_managedby_group2`: Other group to be managed during the tests
+- `win_domain_groups_managedby_manager`: Manager of the test group
+- `win_domain_groups_managedby_manager2`: Other manager of the test group
+- `win_domain_groups_managedby_domain_server`: Specifies the domain server where the Active Directory is modified.
+
+One way to provide all the previous information is calling the testing playbook passing the host to use and an additional vault inventory plus the default one provided for testing, as it's show in this example:
 
 ```shell
-$ cd thisrole/tests
-$ ansible-playbook main.yml
+$ cd amtega.win_domain_groups/tests
+$ ansible-playbook main.yml -e "win_domain_groups_managedby_tests_host=test_host" -i inventory -i ~/mycustominventory.yml --vault-id myvault@prompt
 ```
 
 ## License
 
-Copyright (C) <!-- YEAR --> AMTEGA - Xunta de Galicia
+Copyright (C) 2019 AMTEGA - Xunta de Galicia
 
 This role is free software: you can redistribute it and/or modify it under the terms of:
 
@@ -100,6 +78,4 @@ This role is distributed in the hope that it will be useful, but WITHOUT ANY WAR
 
 ## Author Information
 
-- <!-- author _name 1 -->.
-- <!-- author _name 2 -->.
-- <!-- author _name N -->.
+- Daniel Sánchez Fábregas.
---
+++ library/win_domain_group_managedby.ps1
@@ -0,0 +1,281 @@
+#!powershell
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+Set-StrictMode -Version 2.0
+
+$spec = @{
+    options = @{
+        domain_server = @{ type = "str" }
+        group = @{ type = "str"; required = $true }
+        manager = @{ type = "str" }
+        manage_enabled = @{ type = "bool" }
+        state = @{ type = "str"; choices = "absent", "present", "query"; default = "present" }
+    }
+    supports_check_mode = $true
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+
+
+$domain_server = $module.Params.domain_server
+$group = $module.Params.group
+$manager = $module.Params.manager
+$manage_enabled = $module.Params.manage_enabled
+$state = $module.Params.state
+
+$extra_args = @{}
+If ($null -ne $domain_server) {
+  $extra_args.Server = $domain_server
+  $server_drive = "SERVER"
+  Try {
+    Import-Module ActiveDirectory
+  }
+  Catch {
+     Fail-Json $result "Failed to import ActiveDirectory PowerShell module."
+  }
+  New-PSDrive -Name $server_drive -PSProvider ActiveDirectory -Server $domain_server -Scope Global -root "//RootDSE/" | Out-Null
+} Else {
+  $server_drive = "AD"
+}
+
+If ($manager -eq $null -and $state -eq 'present') {
+    $module.FailJson("Parameter 'manager' is required when state='$state'")
+}
+If ($manager -ne $null -and $state -ne 'present') {
+    $module.FailJson("Parameter 'manager' must be empty when state='$state'")
+}
+
+If ($manager -eq $null -and $manage_enabled -eq $true) {
+  $module.FailJson("Parameter 'manager' is required when manage_enabled=Yes")
+}
+
+If ($manage_enabled -eq $null) {
+  If ($state -eq 'present') {
+    $manage_enabled = $true
+  } ElseIf ($state -eq 'absent') {
+    $manage_enabled = $false
+  }
+} Else { # $manage_enabled != $null
+  If ($state -eq 'query') {
+    $module.FailJson("Parameter 'manage_enabled' must be empty when state='$state'")
+  }
+}
+
+# $module.Result._debug = @()
+# Function Set-AnsibleDebug($linea) { $module.Result._debug += "[$($MyInvocation.ScriptLineNumber)] $linea" }
+
+# ------------------------------------------------------------------------------
+Function Get-ManagerObject($manager_name) {
+  If ($null -eq $manager_name) { return $null }
+  Try {
+    $m_obj = Get-ADObject -Filter "name -eq '$manager_name'"
+    # $manager_name = $m_obj.DistinguishedName -replace 'CN=([^=]*),.*$','$1'
+    $manager_object = Invoke-Expression "Get-ad$($m_obj.ObjectClass) '$($m_obj.DistinguishedName)'"
+  } Catch {
+    $module.FailJson("Failed reading manager $manager_name data: $($_.Exception.Message)", $_)
+  }
+  return $manager_object
+}
+
+# ------------------------------------------------------------------------------
+Function Get-CurrentState($desired_state) {
+
+  $group_object = Try {
+      Get-AdGroup `
+        -Identity $desired_state.group `
+        -Properties Name,DistinguishedName,ManagedBy `
+         @extra_args
+      # } Catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
+      } Catch {
+        $module.FailJson("Failed reading group $($desired_state.group) data: $($_.Exception.Message)", $_)
+      }
+
+  If ($null -eq $group_object.ManagedBy) {
+    $manager_object = $null
+    $manager = $null
+    $state = "absent"
+  } Else {
+    $manager_name = $group_object.ManagedBy -replace 'CN=([^=]*),.*$','$1'
+    $manager_object = Get-ManagerObject $manager_name
+    $manager = $manager_object.Name
+    $state = "present"
+  }
+
+  $manage_enabled = Get-GroupManagedEnabled $group_object $manager_object
+
+  $initial_state = [ordered]@{
+    group = $desired_state.group
+    manager = $manager
+    manage_enabled = $manage_enabled
+    state = $state
+    _ = @{ # HACK: Private values
+        group_object = $group_object
+        manager_object = $manager_object
+      }
+  }
+
+  return $initial_state
+}
+
+
+# ------------------------------------------------------------------------------
+Function Get-GroupManagedEnabled($group, $manager) {
+  If ($manager) {
+    $manage_enabled = Try {
+        If((Get-Acl -Path "AD:\$($group.DistinguishedName)").sddl.split(")(") `
+          | Where-Object{$_ -match $manager.SID.value }) { $True } Else { $False }
+      } Catch { $null }
+  } Else {
+    $manage_enabled = $False
+  }
+  return $manage_enabled
+}
+
+
+# ------------------------------------------------------------------------------
+Function Set-GroupManagedBy($group, $manager) {
+  If ($null -eq $manager) {
+    Try {
+      Set-ADGroup `
+        -Identity $desired_state.group `
+        -Clear ManagedBy `
+        -WhatIf:$module.CheckMode `
+        @extra_args
+    } Catch {
+      $module.FailJson("Failed to clear ManagedBy attribute in group $($group.Name): $($_.Exception.Message)", $_)
+    }
+  } Else {
+    Try {
+      Set-ADGroup `
+        -Identity $desired_state.group `
+        -ManagedBy $manager `
+        -WhatIf:$module.CheckMode `
+        @extra_args
+    } Catch {
+      $module.FailJson("Failed to set ManagedBy attribute in group $($group.Name): $($_.Exception.Message)", $_)
+    }
+  }
+  $module.Result.changed = $true
+}
+
+# ------------------------------------------------------------------------------
+Function Set-GroupManagedEnabled($group, $manager, $manage_enabled) {
+
+  If ($null -eq $manager) {
+    $module.FailJson("Error: Can't enable a unespecified manager")
+  }
+
+  $extendedrightsmap = @{}
+  Try {
+    $rootdse = Get-ADRootDSE @extra_args
+    Get-ADObject -SearchBase ($rootdse.ConfigurationNamingContext) `
+      -LDAPFilter "(&(objectclass=controlAccessRight)(rightsguid=*))" `
+      -Properties displayName,rightsGuid `
+      @extra_args `
+    | ForEach-Object {$extendedrightsmap[$_.displayName]=[System.GUID]$_.rightsGuid}
+  } Catch {
+    $module.FailJson("Failed reading extended permission descriptions: $($_.Exception.Message)", $_)
+  }
+
+  Try {
+    $acl = Get-Acl -Path "$($server_drive):\$($group.DistinguishedName)"
+  } Catch {
+    $module.FailJson("Failed reading ACL in group $($group.DistinguishedName): $($_.Exception.Message)", $_)
+  }
+  $permission =[System.DirectoryServices.ActiveDirectoryRights]::WriteProperty -bor [System.DirectoryServices.ActiveDirectoryRights]::ExtendedRight
+  $group_SID = [System.Security.Principal.SecurityIdentifier]$($manager.SID)
+  $access_rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $group_SID,$permission,"Allow",$extendedrightsmap["Add/Remove self as member"]
+  If ($manage_enabled) {
+    $acl.AddAccessRule($access_rule)
+  } Else {
+    $acl.RemoveAccessRule($access_rule) | Out-Null
+  }
+
+  Try {
+    Set-ACL -ACLObject $acl -Path "$($server_drive):\$($group.DistinguishedName)" -WhatIf:$module.CheckMode
+  } Catch {
+    $module.FailJson("Failed setting ACL in group $($group.DistinguishedName): $($_.Exception.Message)", $_)
+  }
+  $module.Result.changed = $true
+}
+
+
+# ------------------------------------------------------------------------------
+Function ConvertTo-SerializedState($state) {
+  return @(
+    $state.GetEnumerator() | ForEach-Object {
+      If ($null -ne $_.Value) {
+        "$($_.Name): $($_.Value)`n"
+      } else {
+        ""
+      }
+    } ) -join ''
+}
+
+# ··············································································
+
+$desired_state = [ordered]@{
+  group = $group
+  manager = $manager
+  manage_enabled = $manage_enabled
+  state = $state
+  _ = @{ # HACK: Private values
+      # group_object = $group_object
+      manager_object = Get-ManagerObject $manager
+    }
+}
+
+$initial_state = Get-CurrentState $desired_state
+
+If ($desired_state.state -eq "query") {
+  $initial_state.Remove("_") # Hack (cleaning previous hack)
+  $module.Result.value = $initial_state
+  $module.ExitJson()
+}
+
+
+If (($initial_state.manage_enabled -and -not $desired_state.manage_enabled) -or (($initial_state.manager -ne $desired_state.manager)  -and ($initial_state.manage_enabled -and $desired_state.manage_enabled))) {
+  Set-GroupManagedEnabled $initial_state._.group_object $initial_state._.manager_object $false
+}
+If (($null -ne $initial_state.manager) -and ($initial_state.manager -ne $desired_state.manager)) {
+  Set-GroupManagedBy $initial_state._.group_object $null
+}
+
+If (($null -ne $desired_state.manager) -and ($initial_state.manager -ne $desired_state.manager)) {
+  # FIXME: get $desired_state._.group_object
+  Set-GroupManagedBy $initial_state._.group_object $desired_state.manager
+}
+
+If ((-not $initial_state.manage_enabled -and $desired_state.manage_enabled) `
+    -or (($initial_state.manager -ne $desired_state.manager) `
+         -and ($initial_state.manage_enabled -and $desired_state.manage_enabled))) {
+  Set-GroupManagedEnabled $initial_state._.group_object $desired_state._.manager_object $true
+}
+
+#------------------------------------------------------------------
+
+$initial_state.Remove("_") # Hack (cleaning previous hack)
+
+If ($module.Result.changed) {
+  $final_state = Get-CurrentState $desired_state
+  # $final_state.Remove("_") # Hack (cleaning previous hack)
+} else {
+  $final_state = $initial_state
+}
+
+if ($module.CheckMode) {
+  $after_state  = $desired_state
+} else {
+  $after_state  = $final_state
+}
+
+$after_state.Remove("_") # Hack (cleaning previous hack)
+$module.Result.value = $after_state
+
+
+$module.Diff.before = ConvertTo-SerializedState $initial_state
+$module.Diff.after  = ConvertTo-SerializedState $after_state
+
+$module.ExitJson()
